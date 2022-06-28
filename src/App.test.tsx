5b659369e99e5640ddd84a473017ef9a333e17ef
Mario Cesar <mariocesar@pop-os.localdomain>
2022-06-28 20:44:59 -0300

    teste de falha

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { mount } from "@cypress/react";
 import App from "./App";
+/* eslint-disable jest/valid-expect */
+
 
 it("Checar adição de apenas um produto no carrinho", () => {
   mount(<App />);
   cy.get("#product-0>button").click();
 });
 
-
+it("Teste de caso falho", () => {
+  expect(true).to.equal(false)
+});
 
